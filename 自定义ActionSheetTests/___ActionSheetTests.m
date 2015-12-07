739c4e89c6d1e6583dccf6e91cdf4a8f6987e8c8
Dvel <paldvel@qq.com>
2015-12-07 14:35:19 +0800

    Initial Commit

---
+++ 自定义ActionSheetTests/___ActionSheetTests.m
@@ -0,0 +1,39 @@
+//
+//  ___ActionSheetTests.m
+//  自定义ActionSheetTests
+//
+//  Created by Dvel on 15/12/7.
+//  Copyright © 2015年 Dvel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ___ActionSheetTests : XCTestCase
+
+@end
+
+@implementation ___ActionSheetTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
